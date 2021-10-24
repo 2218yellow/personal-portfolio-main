b4294ee05a6ec977c0cbebcff6127cbed894655d
celeis <gonca.negis@gmail.com>
2021-10-24 11:05:02 +0300

    initial commit
    Co-authored-by: Ilhan Negis <eringen@users.noreply.github.com>

---
+++ src/pages/HomePage.js
@@ -0,0 +1,21 @@
+const HomePage = () => {
+  return (
+    <section className='homePage' id='home'>
+      <div className='section-center'>
+        <h2
+          className='section-title'
+          data-aos='zoom-in'
+          data-aos-duration='1500'
+        >
+          Hi, I'm Gonca
+        </h2>
+        <h3 className='mainText' data-aos='zoom-in' data-aos-duration='2000'>
+          I came here to drink milk and build apps, <br /> and I've just
+          finished my milk.
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
